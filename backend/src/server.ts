d5346315ffb693dc61b3472ec9561d1c1cf32ff8
Lina Phung <z5421675@ad.unsw.edu.au>
2026-03-14 06:09:41 +1100

    testing

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -2,6 +2,8 @@ import express from 'express'
 import type { Request, Response } from 'express'
 import dotenv from 'dotenv'
 
+console.log('MINIMAL SERVER FILE IS RUNNING')
+
 dotenv.config()
 
 const app = express()
